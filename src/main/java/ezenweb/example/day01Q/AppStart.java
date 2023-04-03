201f986136d2a17657a491734f62f4d269aacb45
kimchunma <acttesty44@naver.com>
2023-04-03 17:26:52 +0900

    EE123

--- src/main/java/ezenweb/example/day01Q/AppStart.java
+++ src/main/java/ezenweb/example/day01Q/AppStart.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication // MVC , 내장톰캣, Restful ,스프링 컨테이너 등등
 public class AppStart {
+
+    
     public static void main(String[] args) {
             SpringApplication.run(AppStart.class);
     }
